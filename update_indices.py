2e824bd286e0c5927de8c0fca29e9fa65cae9380
markLee <63534243+mark94081@users.noreply.github.com>
2025-05-08 22:29:12 -0400

    Update update_indices.py

--- update_indices.py
+++ update_indices.py
@@ -86,8 +86,8 @@ def fetch_and_insert_data(symbol, period):
         print(f"Fetching data for {table_name} into table `{table_name}`...")
         ticker = Ticker(str(symbol))  # Ensure symbol is string
         if period == 'max':
-            start_date = '2024-05-01'
-            end_date = '2025-05-08'
+            start_date = '2025-04-24'
+            end_date = '2025-05-07'
             df = ticker.history(start=start_date, end=end_date, auto_adjust=False)#
             print(df.tail())
         else:
